ce5b5017c835e2d7170e57e17f5ab15c7d8d1b0b
Karl Häggbom <karl@doriantull.us>
2022-09-01 10:07:13 +0200

    uncomment tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,8 @@
 
 composer install --prefer-dist --ignore-platform-req=ext-gd
 
-ls
-#cp .env.ci .env
-#
-#php artisan key:generate
-#
-#php artisan test
+cp .env.ci .env
+
+php artisan key:generate
+
+php artisan test
